bb9740cd98273b2cf0f2732c01f6b6cc2d2f9ffe
bronkolie <thabroccolidood@gmail.com>
2024-10-14 14:25:59 +0200

    Applied autoformatter

--- main.py
+++ main.py
@@ -7,48 +7,48 @@ import time
 
 
 # Given parameters
-wingspan = 14 # m
-length = 5.40 # m
-chord = 2 # m
-wing_area = chord * wingspan # m²
+wingspan = 14  # m
+length = 5.40  # m
+chord = 2  # m
+wing_area = chord * wingspan  # m²
 oswald_efficiency = 0.85
-mass = 450 # kg
-min_velocity = 10 # m/s
-max_velocity = 140 # m/s
-begin_altitude = 10000 # m
+mass = 450  # kg
+min_velocity = 10  # m/s
+max_velocity = 140  # m/s
+begin_altitude = 10000  # m
 max_lift_coefficient = 1.5
 
 airfoils = [Airfoil("NACA 0012", "xf-n0012-il-50000.csv"),
-Airfoil("NACA 0009", "xf-n0009sm-il-50000.csv"),
-Airfoil("NACA 2414", "xf-n2414-il-50000.csv"),
-Airfoil("NACA 2415", "xf-n2415-il-50000.csv"),
-Airfoil("NACA 6409", "xf-n6409-il-50000.csv"),
-Airfoil("NACA 0006", "xf-naca0006-il-50000.csv"),
-Airfoil("NACA 0008", "xf-naca0008-il-50000.csv"),
-Airfoil("NACA 0010", "xf-naca0010-il-50000.csv"),
-Airfoil("NACA 0012", "xf-naca0012h-sa-50000.csv"),
-Airfoil("NACA 0015", "xf-naca0015-il-50000.csv"),
-]
+            Airfoil("NACA 0009", "xf-n0009sm-il-50000.csv"),
+            Airfoil("NACA 2414", "xf-n2414-il-50000.csv"),
+            Airfoil("NACA 2415", "xf-n2415-il-50000.csv"),
+            Airfoil("NACA 6409", "xf-n6409-il-50000.csv"),
+            Airfoil("NACA 0006", "xf-naca0006-il-50000.csv"),
+            Airfoil("NACA 0008", "xf-naca0008-il-50000.csv"),
+            Airfoil("NACA 0010", "xf-naca0010-il-50000.csv"),
+            Airfoil("NACA 0012", "xf-naca0012h-sa-50000.csv"),
+            Airfoil("NACA 0015", "xf-naca0015-il-50000.csv"),
+            ]
+
 
 # Static parameters
-g = 3.73 # m/s²
+g = 3.73  # m/s²
 air_density = 0.02  # kg/m³
 
 # Calculated parameters
-weight = mass * g # N
-lift = weight # N
-aspect_ratio = wing_area**2 / chord 
+weight = mass * g  # N
+lift = weight  # N
+aspect_ratio = wing_area**2 / chord
 
 
 # Velocity as X-axis
 velocity = np.linspace(10, 240, 1000)  # m/s
-lift_coefficient = lift / (0.5 * air_density *  velocity**2 * wing_area)
+lift_coefficient = lift / (0.5 * air_density * velocity**2 * wing_area)
 
 plots = plt.subplots(1, 3, figsize=(20, 10))[1]
 
 
-
-airfoils.sort(key = lambda x: x.name)
+airfoils.sort(key=lambda x: x.name)
 
 print("Calculating...")
 start_time = time.time()
@@ -61,7 +61,8 @@ for airfoil in airfoils:
         cls = []
         cds = []
         for i, row in enumerate(csv.reader(airfoil_csv)):
-            if i < 11: continue
+            if i < 11:
+                continue
             alpha = float(row[0])
             cl = float(row[1])
             cd = float(row[2])
@@ -69,8 +70,6 @@ for airfoil in airfoils:
             cls.append(cl)
             cds.append(cd)
 
-
-    
     angle_of_attack = [None for _ in range(len(velocity))]
     zero_lift_drag_coefficient = [np.nan for _ in range(len(velocity))]
     for i, needed_cl in enumerate(lift_coefficient):
@@ -81,9 +80,9 @@ for airfoil in airfoils:
                 min_alpha = alpha
                 alpha_index = j
                 found_alpha = True
-        
+
         if not found_alpha:
-            continue        
+            continue
         bigger_cl = cls[alpha_index]
         bigger_alpha = alphas[alpha_index]
 
@@ -95,10 +94,11 @@ for airfoil in airfoils:
 
         smaller_alpha = alphas[alpha_index-1]
         smaller_cl = cls[alpha_index-1]
-        
+
         lerp_amount = (needed_cl - smaller_cl) / (bigger_cl - smaller_cl)
-        lerp_alpha = smaller_alpha + lerp_amount * (bigger_alpha - smaller_alpha)
-        
+        lerp_alpha = smaller_alpha + lerp_amount * \
+            (bigger_alpha - smaller_alpha)
+
         smaller_cd = cds[alpha_index - 1]
         bigger_cd = cds[alpha_index]
         lerp_cd = smaller_cd + lerp_amount * (bigger_cd - smaller_cd)
@@ -108,13 +108,15 @@ for airfoil in airfoils:
 
     zero_lift_drag_coefficient = np.array(zero_lift_drag_coefficient)
 
-    induced_drag_coefficient = lift_coefficient**2 / (np.pi * oswald_efficiency * aspect_ratio)
+    induced_drag_coefficient = lift_coefficient**2 / \
+        (np.pi * oswald_efficiency * aspect_ratio)
     total_drag_coefficient = zero_lift_drag_coefficient + induced_drag_coefficient
     lift_drag_ratio = lift_coefficient / total_drag_coefficient
     glide_ratio = lift_drag_ratio
-    glide_distance = glide_ratio * begin_altitude # m
-    glide_pythagorean_distance = np.sqrt(glide_distance ** 2 + begin_altitude ** 2) # m
-    glide_time = glide_pythagorean_distance / velocity # s
+    glide_distance = glide_ratio * begin_altitude  # m
+    glide_pythagorean_distance = np.sqrt(
+        glide_distance ** 2 + begin_altitude ** 2)  # m
+    glide_time = glide_pythagorean_distance / velocity  # s
 
     plots[0].plot(velocity, glide_distance / 1000, label=airfoil.name)
     plots[1].plot(velocity, glide_time / 60, label=airfoil.name)
